1d61cf0d3f180b359316f489777e992bf1748165
BobyHar <84946161+BobyHar@users.noreply.github.com>
2023-01-19 11:29:08 +0700

    Update training.py

--- training.py
+++ training.py
@@ -33,7 +33,101 @@ def app():
         st.subheader('Data Frame')
         st.dataframe(df)
 
+        st.write("Distribution of Orders (Dependent variable)")
+
+        st.write((df["ID"].value_counts()))
+
+        st.write(df.head())
+
+        st.write(df.info())
+
+        df.isna().sum()
+
+        df = df.fillna(float(df['Income'].mean()))
+        df['Income'].isna().sum()
+
+        df.describe()
+
+        df.info()
         
+        numerics = ['int64', 'float64']
+        numericColumns = df.select_dtypes(include=numerics)
+        # print(numericColumns)
+
+        plt.figure(1 , figsize = (45 , 90))
+        n = 0
+        for x in numericColumns:
+            n += 1
+            plt.subplot(8 , 8 , n)
+            plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+            sns.boxplot(x = df[x])
+        plt.show()
+
+        df = df[(df["Year_Birth"] > 1939)]
+        df = df[(df["Income"]<100000)]
+        df = df[(df["MntMeatProducts"]<1500)]
+        df = df[(df["MntSweetProducts"]<250)]
+        df = df[(df["NumDealsPurchases"]<10)]
+        df = df[(df["NumWebPurchases"]<10)]
+        df = df[(df["NumCatalogPurchases"]<10)]
+
+        plt.figure(1 , figsize = (45 , 90))
+        n = 0
+        for x in numericColumns:
+            n += 1
+            plt.subplot(8 , 8 , n)
+            plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+            sns.boxplot(x = df[x])
+        plt.show()
+
+        corrmat= df.corr()
+        plt.figure(figsize=(20,20))  
+        sns.heatmap(corrmat,annot=True, center=0)
+
+        df.describe()
+
+
+        print(df["Education"].value_counts())
+
+        df["Education"] = df["Education"].replace({"Graduation":"Sudah Lulus", "PhD":"Sudah Lulus", "Master":"Sudah Lulus", "2n Cycle":"Belum Lulus", "Basic":"Belum Lulus"})
+
+        df["Marital_Status"].value_counts()
+
+        df['Marital_Status'] = df["Marital_Status"].replace({"Married":"Pasangan", "Together":"Pasangan", "Absurd":"Sendiri", "Widow":"Sendiri", "YOLO":"Sendiri", "Divorced":"Sendiri", "Single":"Sendiri", "Alone":"Sendiri"})
+
+        df["Dt_Customer"] = pd.to_datetime(df["Dt_Customer"])
+
+        df["Age"] = (2023-df["Year_Birth"]).round()
+
+        df["TotalSpending"] = df["MntWines"] + df["MntFruits"]+ df["MntMeatProducts"] + df["MntFishProducts"] + df["MntSweetProducts"] + df["MntGoldProds"]
+
+        df["Children"] = df["Kidhome"] + df["Teenhome"]
+
+        df["PeopleAtHome"] = (df["Marital_Status"].replace({"Sendiri": 1, "Pasangan":2}).astype(int)) + (df["Children"]).astype(int)
+
+        df["Parent"] = np.where(df.Children> 0, 1, 0)
+
+        df_copy = df.copy()
+
+        df_copy = df_copy.drop(["Z_CostContact", "Z_Revenue", "ID", "Year_Birth",'AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'AcceptedCmp1','AcceptedCmp2', 'Complain', 'Response', 'Dt_Customer', 'Recency'], axis=1)
+
+        df_copy.info()
+
+        # Label Encoding
+        objectFeature = (df_copy.dtypes == 'object')
+        objectFeature = list(objectFeature[objectFeature].index)
+         #st.write((objectFeature))
+
+        label = LabelEncoder()
+        for i in objectFeature:
+            df[i] = df[[i]].apply(label.fit_transform)
+            df_copy[i] = df_copy[[i]].apply(label.fit_transform)
+
+        # Scaling
+        scale = StandardScaler()
+        scale.fit(df_copy)
+        scaled_ds = pd.DataFrame(scale.transform(df_copy),columns= df_copy.columns)
+
         st.write("""MODELING""")
 
         # Menentukan jumlah kluster dengan metode elbow
@@ -49,7 +143,6 @@ def app():
         plt.xlabel('Number of Clusters') , plt.ylabel('Inertia')
         plt.show()
 
-
         #Initiating the KMeans model 
         fig, ax = plt.subplots()
         km = KMeans(n_clusters = 4, init='k-means++', n_init = 10 , max_iter=300, random_state= 111)
@@ -64,8 +157,8 @@ def app():
         #Plotting countplot of clusters
         pal = ["#FD8A8A","#F1F7B5", "#A8D1D1","#9EA1D4"]
         pl = sns.countplot(x=scaled_ds["Cluster"], palette= pal)
-        
 
+        scaled_ds.info()
 
         fig, ax = plt.subplots()
         pl = sns.scatterplot(data = scaled_ds, x = scaled_ds["TotalSpending"], y = scaled_ds["Income"], hue = scaled_ds["Cluster"], palette= pal)
@@ -77,7 +170,7 @@ def app():
             color="yellow",
             zorder=10,
         )
-        pl.set_title("Total spending vs total income")
+        pl.set_title("Total spending vs income")
         plt.legend()
         plt.show()
         st.pyplot(fig)
@@ -125,7 +218,6 @@ def app():
         plt.figure()
         sns.jointplot(x=df['Parent'], y=df["TotalSpending"], hue =df["Cluster"], kind="kde", palette=pal)
         st.pyplot(plt.show())
-     
 
 if __name__=='__main__':
     app()
